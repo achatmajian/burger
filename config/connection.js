727bbfbe641e8c3dad3975af0048b2aed3abb4ac
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-01-08 21:46:56 -0500

    fixed aws connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require ("mysql");
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: "database-burger.cbsysftdozvr.us-east-2.rds.amazonaws.com",
+    user: "arek",
     password: "password",
     database: "burgers_db"
 });
--- db/burgers_db.sql
+++ db/burgers_db.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger_name varchar(100) NOT NULL,
-  devoured boolean. 
+  devoured boolean, 
   PRIMARY KEY (id)
 );
