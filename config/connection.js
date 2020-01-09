15f5fd2df39afb4be26509257e27214c85d1b82a
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-01-08 22:14:59 -0500

    jawsdb thing we did

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
 var mysql = require ("mysql");
-var connection = mysql.createConnection({
-    host: "database-burger.cbsysftdozvr.us-east-2.rds.amazonaws.com",
-    user: "arek",
-    password: "password",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "database-burger.cbsysftdozvr.us-east-2.rds.amazonaws.com",
+        user: "arek",
+        password: "password",
+        database: "burgers_db"
+})
+};
+
+
 
 connection.connect();
 
